8697a58a4afee2f887771ea6531a06b4817f79be
Eltlwany <144901606+Eltlwany@users.noreply.github.com>
2025-11-13 03:52:26 +0300

    Create task-manager.js

---
+++ task-manager.js
@@ -0,0 +1,13 @@
+class TaskManager {
+    constructor(app) {
+        this.app = app;
+    }
+
+    async renderTasks() {
+        // كود renderDynamicTasks من ملفك الأصلي
+    }
+
+    async handleTaskClaim(e) {
+        // كود handleClaimTask من ملفك الأصلي
+    }
+}
